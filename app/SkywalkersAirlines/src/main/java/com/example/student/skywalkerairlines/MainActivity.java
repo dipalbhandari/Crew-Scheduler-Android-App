aef492c375ee3b1c1aaf3e8f0a3b0af8ab533960
rahul.prajapati90904 <rahul.prajapati90904@gmail.com>
2018-10-31 17:19:14 -0500

    layout fix

--- app/SkywalkersAirlines/src/main/java/com/example/student/skywalkerairlines/MainActivity.java
+++ app/SkywalkersAirlines/src/main/java/com/example/student/skywalkerairlines/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
     }
 }
